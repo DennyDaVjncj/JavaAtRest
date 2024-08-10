5bc62889deedf888d68c9307fb46e47711d3c456
DennyDaVjncj <daVjncjCode@gmail.com>
2024-08-10 14:22:45 -0400

    connecting full project to cloud

---
+++ JavaAtRest/src/test/java/io/github/dennydavjncj/JavaAtRest/JavaAtRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dennydavjncj.JavaAtRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAtRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
